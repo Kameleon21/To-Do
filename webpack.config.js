bd85edd20d88284bd60fed9ce8850e8bd39666c8
Kameleon21 <kamilrogozinski29@gmail.com>
2023-06-23 08:42:00 +0100

    created project file structure and set up webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main[contenthash].js",
+    clean: true,
+  },
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: "To-Do",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+};
